18b237845b7a3481405a7daf657ab25db523e6b6
Badredddine Bendriss <badrddin.bendriss@gmail.com>
2021-09-10 23:17:16 +0100

    first commit

---
+++ src/main/kotlin/com/example/gsaccessingdatajpa/GsAccessingDataJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gsaccessingdatajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsAccessingDataJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<GsAccessingDataJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gsaccessingdatajpa/GsAccessingDataJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gsaccessingdatajpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsAccessingDataJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
